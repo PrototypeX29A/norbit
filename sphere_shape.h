7456e82bc47719338a7255740fdd8ab7f810958b
Peter Reineke <preineke@gmx.com>
2010-07-10 00:21:52 +0200

    adding sphere shape and merge

---
+++ sphere_shape.h
@@ -0,0 +1,12 @@
+#include "game_object.h"
+
+class sphere_shape : public shape
+{
+	public:
+		sphere_shape(float size);
+		sphere_shape(float size, float red, float green, float blue);
+		virtual void draw();
+		virtual ~sphere_shape();
+	private:
+		float size, red, green, blue;
+};
